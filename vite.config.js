8844f514787edabc3c3f6239bba2aa2e9626c580
Thomas Garcia <thomasgarcia42@gmail.com>
2025-05-10 20:25:15 +0900

    Mise à jour de composants

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';   // <-- à ajouter si absent
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: '/CIDAO/', // ← ESSENTIEL pour GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), // <-- ALIAS
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
